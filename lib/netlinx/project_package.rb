3026f453f8ab5b31f789fea3bd8fe94262bf462f
Alex McLain <alex@alexmclain.com>
2013-08-27 23:14:05 -0700

    Created ProjectPackage.

---
+++ lib/netlinx/project_package.rb
@@ -0,0 +1,34 @@
+require 'zip/zip'
+
+module NetLinx
+  class ProjectPackage
+    attr_accessor :excluded_extensions
+    
+    # Parameters:
+    #
+    #   Mode:
+    #     :standard - Intelligently package all files pertaining to a
+    #       NetLinx project.
+    #
+    #     :classic  - Emulates the AMX file packager, including only .axs
+    #       and .axi files in the package. Also mimicks the same folder
+    #       structure.
+    #
+    def initialize(**kvargs)
+      @file                = kvargs.fetch :file, ''
+      @mode                = kvargs.fetch :mode, :standard
+      @excluded_extensions = kvargs.fetch :excluded_extensions,
+        [
+          'ai', 'bmp', 'eps', 'gz', 'jpg', 'jpeg', 'png', 'psd', 'src',
+          'svg', 'tar', 'zip'
+        ]
+    end
+    
+    def pack
+    end
+    
+    def unpack
+    end
+    
+  end
+end
---
+++ test/unit/project_package_test.rb
@@ -0,0 +1,53 @@
+require 'netlinx/project_package'
+require 'test_helper'
+
+describe NetLinx::ProjectPackage do
+  
+  before do
+    @project_package = @object = NetLinx::ProjectPackage.new
+  end
+  
+  after do 
+    @project_package = @object = nil
+  end
+  
+  it "bundles common project files" do
+    # .apw, .axi, .axs, .ir, .jar, .kpd, .tkn, .tko, .tp4
+    skip
+  end
+  
+  it "can exclude files from the file search" do
+    # .ai, .bmp, .eps, .jpg, .jpeg, .png, .psd, .src, .svg
+    #
+    # Just tack this onto a glob so specific file names can also
+    # be excluded.
+    skip
+  end
+  
+  it "creates a 'Read This File' warning file in the package" do
+    skip
+  end
+  
+  it "removes the 'Read This File' warning file when unpacked with this utility" do
+    skip
+  end
+  
+  it "copies and renames a .src file to .zip for easy browsing without extraction" do
+    skip
+  end
+  
+  it "unpacks .src files" do
+    skip
+  end
+  
+  it "flattens the file tree when unpacking a .src file in classic mode" do
+    skip
+  end
+  
+  it "can read a .netlinx-package file for config info" do
+    # One file should be in the user folder for defaults, another should be in
+    # the project folder for overriding defaults.
+    skip
+  end
+  
+end
